Fixed another small errors in overlap derivative routine

eduHF/scf.py
@@ -148,7 +148,7 @@ class SCF:
     def overlap_der_matrix(self, center, dim):
         nbf = self.basis.nbf
         S_der = np.zeros((nbf,nbf))
-        for mu in range(1, nbf):
+        for mu in range(nbf):
             for nu in range(mu+1, nbf):
                 if self.basis(mu).center == center:
                     S_der[mu, nu] = SCF.overlap_der_item(self.basis(mu), self.basis(nu), 0, dim)